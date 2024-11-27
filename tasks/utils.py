ef6bff2f34fd70e2a91927c493c6f0ccb53ec611
Waiss Azizian <waiss.azizian@univ-grenoble-alpes.fr>
2024-11-27 19:51:08 +0100

    feat: Add val_batch_size

--- tasks/utils.py
+++ tasks/utils.py
@@ -4,8 +4,15 @@ import torch.utils.data as data
 
 def to_dataloaders(func):
     def new_func(
-        *args, batch_size: int = 256, pin_memory: bool = False, num_workers: int = -1, **kwargs
+        *args,
+        batch_size: int = 256,
+        val_batch_size: int = None,
+        pin_memory: bool = False,
+        num_workers: int = -1,
+        **kwargs
     ):
+        if val_batch_size is None:
+            val_batch_size = batch_size
         new_kwargs = {"batch_size": batch_size, **kwargs}
         train_dataset, val_dataset = func(*args, **new_kwargs)
         train_dataloader = data.DataLoader(
@@ -17,7 +24,7 @@ def to_dataloaders(func):
         )
         val_dataloader = data.DataLoader(
             val_dataset,
-            batch_size=batch_size,
+            batch_size=val_batch_size,
             shuffle=True,
             num_workers=num_workers,
             pin_memory=pin_memory,
